2bbcb919cb7b0650b7cb6a83c4057abad5be391c
Ward <wardpearce@pm.me>
2024-06-22 15:58:25 +1200

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,7 @@
 
   const downloadUrl =
     "https://github.com/Materialious/Materialious/releases/download/";
-  const latestVersion = "1.0.11";
+  const latestVersion = "1.1.1";
 
   const platforms = [
     {
