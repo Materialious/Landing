3578c7b6589a842bc21fa1f44d3cd970fcec4e9b
WardPearce <wardpearce@pm.me>
2026-01-22 00:04:54 +1300

    Implemeneted swiper

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,28 @@
+<script lang="ts">
+	import { onDestroy, onMount } from 'svelte';
+	import 'swiper/css';
+	import 'swiper/css/navigation';
+	import Swiper from 'swiper';
+	import { Autoplay } from 'swiper/modules';
+
+	let swiper: Swiper | undefined;
+	onMount(() => {
+		swiper = new Swiper('.swiper', {
+			modules: [Autoplay],
+			loop: true,
+			autoplay: {
+				delay: 5000,
+				disableOnInteraction: true
+			},
+			enabled: true
+		});
+	});
+
+	onDestroy(() => {
+		if (swiper) swiper.destroy();
+	});
+</script>
+
 <article class="transparent header">
 	<h1>Welcome to Materialious</h1>
 	<p style="font-size: 1.3em;">
@@ -6,12 +31,46 @@
 		Desktop, Android, and Android TV.
 	</p>
 	<div class="space"></div>
-	<img id="home-preview" src="/previews/home-preview.png" alt="Preview of Materialious home page" />
+
+	<div class="swiper">
+		<div class="swiper-wrapper">
+			<div class="swiper-slide">
+				<img src="/previews/home-preview.png" alt="Preview of Materialious home page" />
+			</div>
+			<div class="swiper-slide">
+				<img src="/previews/player-preview.png" alt="Preview of Materialious player" />
+			</div>
+			<div class="swiper-slide">
+				<img src="/previews/setting-preview.png" alt="Preview of Materialious player" />
+			</div>
+			<div class="swiper-slide">
+				<img src="/previews/chapter-preview.png" alt="Preview of Materialious player" />
+			</div>
+		</div>
+	</div>
 </article>
 
 <style>
-	#home-preview {
+	.swiper {
 		width: min(70%, 1920px);
+	}
+
+	.swiper-button-prev,
+	.swiper-button-next {
+		color: var(--primary);
+	}
+
+	.swiper-slide {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.swiper-slide img {
+		max-width: 100%;
+		max-height: 100%;
+		height: auto;
+		object-fit: contain; /* key line */
 	}
 
 	.header {
@@ -22,7 +81,7 @@
 	}
 
 	@media screen and (max-width: 700px) {
-		#home-preview {
+		.swiper {
 			width: 100%;
 		}
 	}
